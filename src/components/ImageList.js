3b363773772cfe2ba7140faf5dae47c75b485ccb
Sean Jaculina <sjaculina@horizon.csueastbay.edu>
2021-08-04 17:24:33 -0700

    destructured the images map method

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,10 @@
-import react from 'react';
+import React from 'react';
 
 const ImageList = (props) =>{
 
-   const images =  props.images.map((image) =>{
-        return <img src={image.urls.regular} alt="images"/>
-    })
+   const images =  props.images.map((({description, id, urls}) =>{
+        return <img key={id} src={urls.regular} alt={description}/>
+    }));
 
 
     console.log(props);
